8b14ed7b496ab024822549c8ee0fa4c99a339cb8
Дракин Марк <drakin.mark@yandex.ru>
2023-07-23 20:08:20 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,19 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+
+    @Test
+    public void testAdd() {
+        final int expected = 1;
+        final String argumentNumber = "1111111";
+        final String argumentNane = "aaa";
+        PhoneBook phoneBook = new PhoneBook();
+
+        final int result = phoneBook.add(argumentNane,argumentNumber);
+
+        Assertions.assertEquals(expected, result);
+    }
 }
